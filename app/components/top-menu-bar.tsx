ad18e23e8604e57ade51de3eed5f9258b6a60a39
Jack Tanner <jack@jackandtheblockstalk.com>
2025-11-10 14:31:59 +0100

    feat: added in the top menu bar and app switcher and icons

---
+++ app/components/top-menu-bar.tsx
@@ -0,0 +1,146 @@
+"use client";
+
+import React, { useContext, useEffect, useState } from "react";
+import ArrowForwardIcon from "../icons/ArrowForward.svg";
+import TonomyLogo from "../icons/appSwitcherIcons/tonomy.png";
+import "./top-menu-bar.scss";
+// import {
+//     AppsExternalUser,
+//     ExternalUser,
+//     isErrorCode,
+//     SdkErrors,
+// } from "@tonomy/tonomy-id-sdk";
+// import { AuthContext } from "../../tonomyAppList/providers/AuthProvider";
+// import LogoutIcon from "../icons/Logout.svg'";
+import KeyboardArrowDownIcon from "../icons/KeyboardArrawDown.svg";
+import KeyboardArrowUpIcon from "../icons/KeyboardArrowUp.svg";
+import AppSwitcherIcon from "../icons/app-switcher.png";
+import AppSwitcher from "./app-switcher";
+// import useErrorStore from "../../common/stores/errorStore";
+import Debug from "debug";
+const debug = Debug("tonomy-app-websites:accounts:pages:Login");
+
+const TopMenuBar = () => {
+  // const { signout, signin } = useContext(AuthContext);
+  // const [username, setUsername] = useState<string>("");
+  const [showSwitcher, setShowSwitcher] = useState(false);
+  // const [open, setOpen] = useState(false);
+  // const errorStore = useErrorStore();
+  const username = false; // placeholder for username state
+
+  // useEffect(() => {
+  //     async function authentication() {
+  //         try {
+  //             const externalUser = await AppsExternalUser.getUser({
+  //                 autoLogout: false,
+  //             });
+  //             debug("externalUser", externalUser);
+  //             if (externalUser) {
+  //                 signin(externalUser);
+  //                 const uname = await externalUser.getUsername();
+  //                 if (!uname) throw new Error("No username found");
+  //                 setUsername(uname.getBaseUsername());
+  //             } else {
+  //                 setUsername("");
+  //             }
+  //         } catch (e) {
+  //             console.log("e", e);
+  //             if (
+  //                 isErrorCode(e, [
+  //                     SdkErrors.AccountNotFound,
+  //                     SdkErrors.UserNotLoggedIn,
+  //                     SdkErrors.AccountDoesntExist,
+  //                 ])
+  //             ) {
+  //                 setUsername("");
+  //             } else {
+  //                 errorStore.setError({ error: e, expected: false });
+  //             }
+  //         }
+  //     }
+  //     authentication();
+  // }, []); // watch for changes
+
+  // function handleLogout() {
+  //     signout();
+  //     setUsername("");
+  // }
+
+  async function onButtonPress() {
+    // ExternalUser.loginWithTonomy({
+    //     callbackPath: '/callback',
+    //     dataRequest: { username: true },
+    // });
+  }
+
+  return (
+    <div className="tonomy-header">
+      <div className="tonomy-title">
+        <a
+          href={window.location.origin}
+          className="tonomy-title"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <img
+            src={TonomyLogo.src}
+            alt="Tonomy Logo"
+            className="tonomy-logo"
+            width={37}
+            height={37}
+          />
+          <h1 className="tonomy-main-title">Tonomy</h1>
+        </a>
+      </div>
+      <div className="tonomy-time-container">
+        <div className="switcher-container">
+          <img
+            src={AppSwitcherIcon.src}
+            alt="App Switcher"
+            className="tonomy-logo cursor-pointer"
+            width={18}
+            height={18}
+            onClick={() => setShowSwitcher(!showSwitcher)}
+          />
+          {showSwitcher && <AppSwitcher />}
+        </div>
+
+        {username ? (
+          // <div className="dropdown">
+          //     {/* Trigger */}
+          //     <div className="dropdown-trigger" onClick={() => setOpen(!open)}>
+          //         <span className="username">@{username}</span>
+          //         {open ? (
+          //             <KeyboardArrowUpIcon className="arrow" />
+          //         ) : (
+          //             <KeyboardArrowDownIcon className="arrow" />
+          //         )}
+          //     </div>
+
+          //     {/* Dropdown menu */}
+          //     {open && (
+          //         <div className="dropdown-menu cursor-pointer">
+          //             <button className="dropdown-item" onClick={handleLogout}>
+          //                 Log out
+          //                 <LogoutIcon className="icon" />
+          //             </button>
+          //         </div>
+          //     )}
+          // </div>
+          <> </>
+        ) : (
+          <>
+            <span onClick={() => onButtonPress()} className="tonomy-time">
+              Login
+            </span>
+            <ArrowForwardIcon
+              onClick={() => onButtonPress()}
+              className="tonomy-arrow-icon cursor-pointer"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TopMenuBar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import { Home } from "./components/home";
+import TopMenuBar from "./components/top-menu-bar";
 
 export default async function App() {
-  return <Home />;
+  return (
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <TopMenuBar />
+      <div style={{ flex: 1 }}>
+        <Home />
+      </div>
+    </div>
+  );
 }
